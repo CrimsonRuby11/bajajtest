69c996115382b389e198f7f909929389a6578a92
CrimsonRuby11 <skprithviraj8@gmail.com>
2025-08-29 11:09:49 +0530

    port fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bfhlRoutes = require('./routes/bfhl_routes');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
